4e3fa11031aae3a7ae4cc6789d0865afafea62b0
Kaito Suzuki <snails8d@gmail.com>
2023-01-15 16:52:14 +0900

    add: mock endpoint

---
+++ backend/src/adapter/sample.ts
@@ -0,0 +1,26 @@
+import { RouterContext, helpers } from "../../deps.ts";
+
+export const taskHandler = {
+  getAll(ctx: RouterContext) {
+    ctx.response.body = 'Get All Tasks'
+  },
+
+  get(ctx: RouterContext) {
+    const { id } = helpers.getQuery(ctx, { mergeParams: true });
+    ctx.response.body = `Get Task By ID: ${id}`
+  },
+
+  create(ctx: RouterContext) {
+    ctx.response.body = 'Create Task'
+  },
+
+  update(ctx: RouterContext) {
+    const { id } = helpers.getQuery(ctx, { mergeParams: true });
+    ctx.response.body = `Update Task By ID: ${id}`
+  },
+
+  delete(ctx: RouterContext) {
+    const { id } = helpers.getQuery(ctx, { mergeParams: true });
+    ctx.response.body = `Delete Task By ID: ${id}`
+  }
+}
--- backend/src/endpoint/router.ts
+++ backend/src/endpoint/router.ts
@@ -1,4 +1,5 @@
 import { Router, Context } from "../../deps.ts";
+import { taskHandler } from "../adapter/sample.ts";
 
 const router = new Router();
 
@@ -14,5 +15,11 @@ router.get(`/`, async (ctx: Context) => {
   console.log('----test-get------------------');
   setResponseData(ctx, 'test');
 })
+
+router.get('/api/v1/books', taskHandler.getAll);
+router.get('/api/v1/books/:id', taskHandler.get);
+router.post('/api/v1/books', taskHandler.create);
+router.put('/api/v1/books/:id', taskHandler.update);
+router.delete('/api/v1/books/:id', taskHandler.delete);
 
 export { router }
