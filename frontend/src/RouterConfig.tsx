450a1f5dc50b1b2c9410c5f2d14f48dd0cecc18c
Kaito Suzuki <snails8d@gmail.com>
2023-01-15 15:40:45 +0900

    add: login page design

--- frontend/src/RouterConfig.tsx
+++ frontend/src/RouterConfig.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { DashboardPage } from "./pages/Dashboard";
+import Login from "./pages/Login";
 
 import { NotFound } from "./pages/NotFound/NotFound";
 
+export const RouteLogin = '/login' as const; 
 export const RouterConfig:React.FC =() => {
   return (
     <>
@@ -11,6 +13,7 @@ export const RouterConfig:React.FC =() => {
         <Routes>
           <Route index element={<DashboardPage />} />
           <Route path="*" element={<NotFound />} />
+          <Route path={RouteLogin} element={<Login />} />
         </Routes>
       </BrowserRouter>
     </>
